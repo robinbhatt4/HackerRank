adddf86e14180b8e27cc6002af3da132fde2833e
Robin Bhatt <robinbhatt4@gmail.com>
2024-09-09 13:19:44 +0530

    First commit

---
+++ Java Map.java
@@ -0,0 +1,33 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+        Map<String,Integer> phonedir=new HashMap<>();
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+			in.nextLine();
+            phonedir.put(name,phone);
+		}
+        
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            if(phonedir.containsKey(s)){
+                System.out.println(s+"="+phonedir.get(s));
+            }
+            else
+            System.out.println("Not found");
+		}
+	}
+}
+
+
+
